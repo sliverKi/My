2381839691490f87862345fc3a81dc71d3e2fe12
suhyun <id_suhyun@naver.com>
2023-03-07 13:39:30 +0900

    mini

--- idols/serializers.py
+++ idols/serializers.py
@@ -2,30 +2,33 @@ from rest_framework.serializers import ModelSerializer
 
 from .models import Idol, Schedule
 
+
 class ScheduleSerializer(ModelSerializer):
     class Meta:
-        model=Schedule
-        fields=("name", "description")
+        model = Schedule
+        fields = (
+            "pk",
+            "ScheduleTitle",
+            "ScheduleType",
+            "idol_groupName",
+            "idol_name",
+            "participant",  # idol_name (?)
+            "description",
+        )
+
 
 class IdolsListSerializer(ModelSerializer):
     class Meta:
-        model=Idol
-        fields=(
-            "pk",
-            "idol_name",
-            "idol_group",
-            "idol_solo",
-            "idol_profile",
-        )
-        
-        
+        model = Idol
+        fields = ("pk", "idol_solo", "idol_profile", "paticipant,")
+
+
 class IdolDetailSerializer(ModelSerializer):
-    
-    idol_schedule=ScheduleSerializer(
-        read_only=True,#스케줄을 필수 항목으로 인식하지 않음 
-        many=True
+
+    idol_schedules = ScheduleSerializer(
+        read_only=True, many=True  # 스케줄을 필수 항목으로 인식하지 않음
     )
+
     class Meta:
-        model=Idol
-        fields="__all__"
-        
+        model = Idol
+        fields = "__all__"
--- idols/views.py
+++ idols/views.py
@@ -12,25 +12,25 @@ from .serializers import IdolsListSerializer, IdolDetailSerializer, ScheduleSeri
 # api/v1/idols/schedule/1(GET, PUT, DELETE)
 class Idols(APIView):
 
-    permission_classes=[IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get(self, request):
-        all_idols=Idol.objects.all()
-        serializer=IdolsListSerializer(all_idols, many=True)
+        all_idols = Idol.objects.all()
+        serializer = IdolsListSerializer(all_idols, many=True)
         return Response(serializer.data)
-    
-    def post(self, request):#관리자만 허용하게 할 것 
-      #만약 사용자가 관리자가 아니라면 허용하여서는 안됌
-        #if request.user!=user.admin:
-        #raise PermissionDenied
+
+    def post(self, request):  # 관리자만 허용하게 할 것
+        # 만약 사용자가 관리자가 아니라면 허용하여서는 안됌
+        # if request.user!=user.admin:
+        # raise PermissionDenied
 
         serializer = IdolDetailSerializer(data=request.data)
         if serializer.is_valid():
-            idol= serializer.save()
+            idol = serializer.save()
             return Response(IdolsListSerializer(idol).data)
         else:
             return Response(serializer.errors)
-        
+
 
 class IdolDetail(APIView):
     def get_object(self, pk):
@@ -38,13 +38,11 @@ class IdolDetail(APIView):
             return Idol.objects.get(pk=pk)
         except Idol.DoesNotExist:
             raise NotFound
-    
+
     def get(self, request, pk):
-        idol=self.get_object(pk)
+        idol = self.get_object(pk)
         serializer = IdolDetailSerializer(idol)
         return Response(serializer.data)
-    
-
 
 
 class IdolSchedule(APIView):
@@ -55,44 +53,44 @@ class IdolSchedule(APIView):
             raise NotFound
 
     def get(self, request, pk):
-        idol=self.get_object(pk)
-        serializer=ScheduleSerializer(
+        idol = self.get_object(pk)
+        serializer = ScheduleSerializer(
             idol.idol_schedule.all(),
             many=True,
         )
         return Response(serializer.data)
- 
+
 
 class Schedules(APIView):
     def get(self, request):
-        all_schedules=Schedule.objects.all()
-        serializer=ScheduleSerializer(all_schedules, many=True)
+        all_schedules = Schedule.objects.all()
+        serializer = ScheduleSerializer(all_schedules, many=True)
         return Response(serializer.data)
-    
+
     def post(self, request):
         serializer = ScheduleSerializer(data=request.data)
         if serializer.is_valid():
-            schedule= serializer.save()
+            schedule = serializer.save()
             return Response(ScheduleSerializer(schedule).data)
         else:
-            return Response(serializer.errors)    
+            return Response(serializer.errors)
+
 
 class ScheduleDetail(APIView):
-
     def get_object(self, pk):
-        try: 
+        try:
             return Schedule.objects.get(pk=pk)
         except Schedule.DoesNotExist:
             raise NotFound
-        
+
     def get(self, request, pk):
         schedule = self.get_object(pk)
         serializer = ScheduleSerializer(schedule)
-        return Response(serializer.data,)
-    
+        return Response(serializer.data)
+
     def put(self, request, pk):
         schedule = self.get_object(pk)
-        serializer=ScheduleSerializer(
+        serializer = ScheduleSerializer(
             schedule,
             data=request.data,
             partial=True,
@@ -102,7 +100,7 @@ class ScheduleDetail(APIView):
             return Response(ScheduleSerializer(updated_schedule).data)
         else:
             return Response(serializer.errors)
-        
+
     def delete(self, request, pk):
         schedule = self.get_object(pk).delete()
         return Response(status=HTTP_204_NO_CONTENT)
